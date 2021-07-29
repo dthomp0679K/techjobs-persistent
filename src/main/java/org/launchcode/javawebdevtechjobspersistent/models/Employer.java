4b64b20197ea652a3ad5a2447707a54562645d31
dthomp0679K <derek_thompson@cable.comcast.com>
2021-07-29 16:50:59 -0500

    part 3 finished

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,12 +1,17 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
+@OneToMany(mappedBy = "employer")
+private final List<Job> jobs = new ArrayList<>();
 
     @NotBlank
 @Size(min = 3, max = 100)
@@ -24,5 +29,9 @@ private String location;
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 }
